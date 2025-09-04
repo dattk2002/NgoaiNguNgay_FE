de57211aab308419ddea0304c163187234db97e7
linhnt239 <tienlinhnguyen4@gmail.com>
2025-09-04 13:18:52 +0700

    fix: import setupCacheCleanup, initMemory, cleanupMemory

--- src/App.jsx
+++ src/App.jsx
@@ -136,7 +136,9 @@ import RoleBasedRouteGuard from "./components/rbac/RoleBasedRouteGuard";
 import { NotificationProvider, useNotification } from "./contexts/NotificationContext";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
-
+import { setupCacheCleanup } from "./utils/notificationMessages";
+import { initMemoryMonitoring } from "./utils/memoryManager";
+import { cleanupMemory } from "./utils/memoryManager";
 // Import optimized functions
 import {
   getNotificationTitle,
